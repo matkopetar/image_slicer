0f7d18bd79ce82dff5ae3b3cd64561878a0cde13
Petar Matko <petar.matko@htecgroup.com>
2022-01-30 02:39:33 +0100

    Create Image component

---
+++ react_app/src/components/Image.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Image({ image, splittedImages }) {
+    const styles = {
+        splittedImagesStyle: {
+            width: 'minContent',
+            display: 'grid',
+            gridTemplateColumns: `repeat(${splittedImages.length}, auto)`,
+            gap: '5px'
+        }
+    };
+
+    return (
+        splittedImages.length ?
+            <div style={styles.splittedImagesStyle}>
+                { splittedImages.map(row => {
+                    return row.map(image => <img src={image}/>)
+                }) }
+            </div> :
+            <div className='image'> <img src={URL.createObjectURL(image)}/></div>
+    );
+};
+ 
+export default Image;
